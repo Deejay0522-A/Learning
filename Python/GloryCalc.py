8727663cdd396958cd522ed8939704053debde1b
Deejay0522-A <djdenisemiami@gmail.com>
2025-11-06 16:43:41 -0500

    calc

---
+++ Python/GloryCalc.py
@@ -0,0 +1,11 @@
+import math
+
+points = 5
+
+gloryNeed = int(input("Enter the glory needed: "))
+
+gloryAcqNum = 100
+winsPerGlory = 20
+
+winsNeedPerGlory = None
+print("Wins needed for glory:", winsNeedPerGlory)
