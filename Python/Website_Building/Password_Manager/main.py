73bcf8de1990763b4f415381bb57ab70d65046ca
Deejay0522-A <djdenisemiami@gmail.com>
2026-02-12 13:29:06 -0500

    PassManagerUpd1

--- Python/Website_Building/Password_Manager/main.py
+++ Python/Website_Building/Password_Manager/main.py
@@ -1,2 +1,27 @@
 from fasthtml.common import *
 from starlette.testclient import TestClient
+
+def render():
+    return Li(f"{users.username} - {users.password} | {A(f"{users.website}", href=users.website)}", 
+              A("Remove", hx_delete=f"/{users.id}", hx_swap="OuterHTML", target_id=f"contact-{users.id}", id=f"contact-{users.id}"))
+
+app, rt, users, Task = fast_app(
+    'users.db',
+    render = render,
+    username = str,
+    passowrd = str,
+    website = str,
+    id = int,
+    pk = "id"
+)
+
+def CreateUsername():
+    return Input(id="username", placeholder="Username")
+
+@rt("/")
+def Home():
+    return P("Hello World!")
+
+
+
+serve()
